0e3d16882cdf0be2942c8320895f92e008434e28
Max Graff <maksymilian.graff@gmail.com>
2025-06-29 21:50:42 +0200

    Configuration class for application constants etc

---
+++ Model/Config.java
@@ -0,0 +1,11 @@
+package Model;
+
+public final class Config {
+
+    public static final int width = 1280;
+    public static final int height = 720;
+    public static final int radius = 180;
+
+    public static final String logoImagePath = "./Assets/white.png";
+    
+}
